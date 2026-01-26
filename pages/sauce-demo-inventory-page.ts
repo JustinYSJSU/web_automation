bb0259e83e487ba4f83da1fbfea4337a42471cd7
JustinYSJSU <justinkaiy02@gmail.com>
2026-01-26 14:32:12 -0800

    initial commit for inventory page POM

---
+++ pages/sauce-demo-inventory-page.ts
@@ -0,0 +1,15 @@
+import { expect, type Locator, type Page} from '@playwright/test';
+
+export class SauceDemoInventoryPage{
+    readonly page: Page;
+    readonly logoutLink: Locator;
+    readonly inventoryContainer: Locator;
+    readonly cartLink: Locator;
+
+    constructor(page: Page){
+        this.page = page;
+        this.logoutLink = page.getByTestId("logout-sidebar-link");
+        this.inventoryContainer = page.getByTestId("inventory-container");
+        this.cartLink = page.getByTestId("shopping-cart-link");
+    }
+}
